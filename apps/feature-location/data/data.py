b0cfef62848f4d379ed7e82fabb15b270af1e5dd
Lukas Schulte <l.schulte@mailbox.org>
2021-03-18 15:53:07 +0100

    implementation for LDA v1

--- apps/feature-location/data/data.py
+++ apps/feature-location/data/data.py
@@ -5,12 +5,18 @@ from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
 
+stop_words = stopwords.words('english')
+
 
 def nltk_filter(doc):
 
     if not ('feature' in doc and 'description' in doc['feature'] and doc['feature']['description']):
         return None
 
-    text_tokens = word_tokenize(doc['feature']['description'])
-    remove_sw = [word for word in text_tokens if word not in stopwords.words()]
-    return ' '.join(remove_sw)
+    text = doc['feature']['description']
+    text = text.lower()
+
+    text_tokens = word_tokenize(text)
+    remove_sw = [word for word in text_tokens if word not in stop_words and word.isalpha()]
+
+    return remove_sw
--- apps/feature-location/models/lda.py
+++ apps/feature-location/models/lda.py
@@ -1,14 +1,29 @@
 import tomotopy as tp
+import pandas as pd
 
 from data import db_commits, data
+
+FILE_NAME = 'lda'
+TOPICS = 20
 
 
 def run():
 
-    mdl = tp.LDAModel(k=20)
-    for word_list in filter(data.nltk_filter, db_commits.find(limit=100)):
+    mdl = tp.LDAModel(k=TOPICS, seed=123)
+
+    data_list = []
+
+    for document in db_commits.find(limit=1000):
+        word_list = data.nltk_filter(document)
         if word_list:
-            mdl.add_doc(word_list)
+            idx = mdl.add_doc(word_list)
+            tmp = {
+                'id': str(document['_id']),
+                'feature': document['feature_id'],
+                'mapping': document['diff'],
+                'model_index': idx
+            }
+            data_list.append(tmp)
 
     for i in range(0, 100, 10):
         mdl.train(10)
@@ -16,10 +31,25 @@ def run():
 
     for k in range(mdl.k):
         print('Top 10 words of topic #{}'.format(k))
-        print(mdl.get_topic_words(k, top_n=10))
+        print(mdl.get_topic_words(k, top_n=3))
 
     mdl.summary()
 
-    print('results:')
+    for row in data_list:
 
-    print(mdl.make_doc(['author' 'email']).get_words(top_n=10))
+        doc = mdl.docs[row['model_index']]
+        topics = doc.get_topics(top_n=TOPICS)
+        topics = sorted(topics, key=lambda item: item[0])
+
+        for t in range(TOPICS):
+            row['topic_{}'.format(t)] = topics[t][1]
+
+    columns = ['id', 'feature', 'mapping', 'model_index']
+    columns.extend(['topic_{}'.format(t) for t in range(TOPICS)])
+    mapping = pd.DataFrame(data_list, columns=columns)
+
+    # print(res)
+
+    mdl.save('{}.mdl'.format(FILE_NAME))
+
+    mapping.to_csv('{}.csv'.format(FILE_NAME))
