f0d9a83da2c93cd193c06b785664afe3f2289636
Lukas Schulte <l.schulte@mailbox.org>
2021-03-19 10:07:22 +0100

    added language_regex for java

--- apps/importer/git/interpreter/__init__.py
+++ apps/importer/git/interpreter/__init__.py
@@ -1,5 +1,8 @@
 LANGUAGE_REGX = {
     'py': {
         'method_name': r'^(\+|\-| )( *|\t*)def (.+)\(.*\):'
+    },
+    'java': {
+        'method_name': r'^(\+|\-| )( *|\t*)(?:public|protected|private|static|\s) +[\w\<\>\[\]]+\s+(\w+) *\([^\)]*\) *(?:\{?|[^;])'
     }
 }
