afb600502b1ab187e54a5781d425c13a22151b70
Lukas Schulte <l.schulte@mailbox.org>
2021-04-02 11:23:00 +0200

    added log2json helper script

---
+++ apps/feature-location/helper/log2json.py
@@ -0,0 +1,24 @@
+import re
+import json
+
+f = open('old_log.txt', 'r')
+
+text = f.read()
+
+logs = re.findall(r'k1: *(\d+) *k2: *(\d+)\n.*\nPA ll per word *(.*)', text)
+
+res = []
+
+for match in logs:
+
+    res.append({
+        'k1': match[0],
+        'k2': match[1],
+        'result': {
+            'train': {
+                'pa': float(match[2])
+            }
+        }
+    })
+
+json.dump(res, open('log2json_res.txt', 'w'), indent=4)
